6fb8d6d0bc3d010fd1198483a399a5f9fde94c24
galibremo <galibremo@gmail.com>
2026-07-31 11:07:10 +0600

    feat: create BrandsModule with associated controller, service, and repository

--- src/modules/brands/brands.module.ts
+++ src/modules/brands/brands.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BrandsController } from './brands.controller';
+import { BrandsService } from './brands.service';
+import { BrandsRepository } from './brands.repository';
 
 @Module({
-    controllers: [BrandsController]
+    controllers: [BrandsController],
+    providers: [BrandsService, BrandsRepository],
 })
 export class BrandsModule { }
+
